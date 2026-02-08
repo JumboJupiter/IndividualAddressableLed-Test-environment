9ee02f1cc5c5a5756382a19c35c04c395450cd5b
JumboJupiter <87942988+JumboJupiter@users.noreply.github.com>
2026-02-08 11:46:35 -0700

    sucessful BW kinda color with letters
    R, G, B, W, Y.  repersented in grid.

--- Testing/PixelSimulator.cpp
+++ Testing/PixelSimulator.cpp
@@ -4,22 +4,53 @@ Francis O'Donovan
 2-07-2026
 
 Simple testing before realworld construction
+orginalt used bianart on off leds now uses full r g b controllable
+leds just like real world product will.
 
 */
 #include <iostream>
 #include <cstdlib>  // system("cls")
 
-const int GRID_WIDTH  = 20;
-const int GRID_HEIGHT = 10;
+struct Color {
+    int r;
+    int g;
+    int b;
+};
 
-bool grid[GRID_HEIGHT][GRID_WIDTH];
+const int GRID_WIDTH  = 36;
+const int GRID_HEIGHT = 12;
+
+//int LoopLegnth; 
+
+Color grid[GRID_HEIGHT][GRID_WIDTH]; //upgraded from bool to show color
 
 void clearGrid() {
     for (int y = 0; y < GRID_HEIGHT; y++) {
         for (int x = 0; x < GRID_WIDTH; x++) {
-            grid[y][x] = false;
+            grid[y][x] = {0, 0, 0}; //insead of "flase" meaning off grid now clears to "black"
         }
     }
+}
+
+void SetAll(int r, int g, int b) {
+    for (int y = 0; y < GRID_HEIGHT; y++) {
+        for (int x = 0; x < GRID_WIDTH; x++) {
+            grid[y][x] = {r, g, b};
+        }
+    }
+}
+
+// allows console to "show" color by using squares
+const char* ColorToSymbol(const Color& c) {
+    if (c.r == 0 && c.g == 0 && c.b == 0) return "· ";
+
+    if (c.r > c.g && c.r > c.b) return "R ";
+    if (c.g > c.r && c.g > c.b) return "G ";
+    if (c.b > c.r && c.b > c.g) return "B ";
+    if (c.r == c.g && c.b == 0) return "Y ";
+    if (c.r == c.b && c.b == c.g) return "W ";
+
+    return "# "; // mixed or white
 }
 
 void printGrid() {
@@ -27,14 +58,18 @@ void printGrid() {
 
     for (int y = 0; y < GRID_HEIGHT; y++) {
         for (int x = 0; x < GRID_WIDTH; x++) {
-            std::cout << (grid[y][x] ? "■ " : "· ");
+            std::cout << ColorToSymbol(grid[y][x]);
         }
         std::cout << "\n";
     }
 }
 
 int main (){
-    printGrid();
     clearGrid();
+    printGrid();
+    
+    SetAll(255,255,255);
+    printGrid();
+
     return 0;
 }
